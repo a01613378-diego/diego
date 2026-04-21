521960bf2856ba7889995e8dbeb134e9dc939e8b
a01613378-diego <a01613378@tec.mx>
2026-04-21 08:34:45 -0600

    Update Corales.py

--- Corales.py
+++ Corales.py
@@ -24,7 +24,7 @@ st.sidebar.header("Parámetros Ambientales")
 temp_input = st.sidebar.slider("Temperatura del Agua (°C)", 24.0, 35.0, 28.0)
 
 # Cargamos el archivo con los datos (.csv)
-df =  pd.read_csv('Corales_ODS14.csv', encoding='latin-1')
+df =  pd.read_csv('Corales_ODS14 (1).csv', encoding='latin-1')
 # Seleccionamos las variables
 X = df[['Temperatura_C']]
 y = df['Porcentaje_Blanqueamiento']
