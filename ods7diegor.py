655193288e835f50aa74a47165b422dfaf6c11a4
a01613378-diego <a01613378@tec.mx>
2026-05-12 08:10:48 -0600

    Add files via upload

---
+++ ods7diegor.py
@@ -0,0 +1,53 @@
+# Importar librerías necearias
+import numpy as np
+import streamlit as st
+import pandas as pd
+
+# Insertamos título
+st.write(''' # ODS 7: ENERGIA PRODUCIDA ''')
+# Insertamos texto con formato
+st.markdown("""
+Esta aplicación utiliza **Machine Learning** para predecir el impacto del calentamiento global
+en los arrecifes de coral, alineado con el **Energía Asequible y No Contaminante**.
+""")
+# Insertamos una imagen
+st.image("7ODS.jfif", caption="ENERGIA PRODUCIDA POR LA INTENSIDAD DE LA RADIACION.")
+
+#st.header('Datos personales')
+# Definimos cómo ingresará los datos el usuario
+# Usaremos un deslizador
+st.sidebar.header("INTENSIDAD")
+# Definimos los parámetros de nuestro deslizador:
+  # Límite inferior: 20kWh. Es el límite inferior donde no hay aire
+  # Límite superior: 70kWh. Es el máximo donde el aire va más rápido
+  # Valor inicial: 105kWh. Considero que es un buen valor que está por el promedio de
+temp_input = st.sidebar.slider("ENERGIS (kWh)", 20.0, 70.0, 105.0)
+
+# Cargamos el archivo con los datos (.csv)
+df =  pd.read_csv('ods7diegoO.csv', encoding='latin-1')
+# Seleccionamos las variables
+X = df[['VAR_2']]
+y = df['VAR_4']
+
+# Creamos y entrenamos el modelo
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+LR = LinearRegression()
+LR.fit(X_train,y_train)
+
+# Hacemos la predicción con el modelo y la temperatura seleccionada por el usuario
+b1 = LR.coef_
+b0 = LR.intercept_
+prediccion = b0 + b1[0]*temp_input
+
+# Presentamos loa resultados
+st.subheader('ENERGIA PRODUCIDA')
+st.write(f'La energia es: {prediccion:.2f}kWh')
+
+if prediccion < 480:
+        st.success("Baja Productividad")
+elif prediccion < 800:
+        st.warning("Productividad media")
+else:
+        st.error("Alta productividad")
